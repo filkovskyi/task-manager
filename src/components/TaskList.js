eb7f29e986db87b472f3802268b61c40f5bf0e72
Serhii Filkovskyi <sfilkovskyi@oroinc.com>
2019-05-28 18:36:31 +0300

    feature/redux -  add form in popup

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,141 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux'
-import {Modal, Button, FormGroup, FormControl, FormLabel} from 'react-bootstrap';
+import {Modal, Button} from 'react-bootstrap';
 import {Formik, Field, Form, Fieldset} from 'formik';
+
+import { withFormik } from 'formik';
+import * as Yup from 'yup';
+import Select from 'react-select';
+
 import Task from './Task';
-import _ from 'lodash';
+
+const formikEnhancer = withFormik({
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email('Invalid email address')
+      .required('Email is required!'),
+    topics: Yup.array()
+      .min(3, 'Pick at least 3 tags')
+      .of(
+        Yup.object().shape({
+          label: Yup.string().required(),
+          value: Yup.string().required(),
+        })
+      ),
+  }),
+  mapPropsToValues: props => ({
+    email: '',
+    topics: [],
+  }),
+  handleSubmit: (values, { setSubmitting }) => {
+    const payload = {
+      ...values,
+      topics: values.topics.map(t => t.value),
+    };
+    setTimeout(() => {
+      alert(JSON.stringify(payload, null, 2));
+      setSubmitting(false);
+    }, 1000);
+  },
+  displayName: 'MyForm',
+});
+
+const MyForm = props => {
+  const {
+    values,
+    touched,
+    dirty,
+    errors,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    handleReset,
+    setFieldValue,
+    setFieldTouched,
+    isSubmitting,
+  } = props;
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email" style={{ display: 'block' }}>
+        Email
+      </label>
+      <input
+        id="email"
+        placeholder="Enter your email"
+        type="email"
+        value={values.email}
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
+      {errors.email &&
+      touched.email && (
+        <div style={{ color: 'red', marginTop: '.5rem' }}>{errors.email}</div>
+      )}
+      <MySelect
+        value={values.topics}
+        onChange={setFieldValue}
+        onBlur={setFieldTouched}
+        error={errors.topics}
+        touched={touched.topics}
+      />
+      <button
+        type="button"
+        className="outline"
+        onClick={handleReset}
+        disabled={!dirty || isSubmitting}
+      >
+        Reset
+      </button>
+      <button type="submit" disabled={isSubmitting}>
+        Submit
+      </button>
+    </form>
+  );
+};
+
+const options = [
+  { value: 'Food', label: 'Food' },
+  { value: 'Being Fabulous', label: 'Being Fabulous' },
+  { value: 'Ken Wheeler', label: 'Ken Wheeler' },
+  { value: 'ReasonML', label: 'ReasonML' },
+  { value: 'Unicorns', label: 'Unicorns' },
+  { value: 'Kittens', label: 'Kittens' },
+];
+
+
+class MySelect extends Component {
+  handleChange = value => {
+    // this is going to call setFieldValue and manually update values.topcis
+    this.props.onChange('topics', value);
+  };
+
+  handleBlur = () => {
+    // this is going to call setFieldTouched and manually update touched.topcis
+    this.props.onBlur('topics', true);
+  };
+
+  render() {
+    return (
+      <div style={{ margin: '1rem 0' }}>
+        <label htmlFor="color">Topics (select at least 3) </label>
+        <Select
+          id="color"
+          options={options}
+          multi={true}
+          onChange={this.handleChange}
+          onBlur={this.handleBlur}
+          value={this.props.value}
+        />
+        {!!this.props.error &&
+        this.props.touched && (
+          <div style={{ color: 'red', marginTop: '.5rem' }}>{this.props.error}</div>
+        )}
+      </div>
+    );
+  }
+}
+
+const MyEnhancedForm = formikEnhancer(MyForm);
 
 class TaskList extends Component {
   constructor(props) {
@@ -67,48 +199,7 @@ class TaskList extends Component {
             <Modal.Title>Add new Task</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <Formik
-              initialValues={{
-                clientName: '',
-                value: '',
-                type: '',
-                gender: ''
-              }}
-              onSubmit={values => {
-                setTimeout(() => {
-                  alert(JSON.stringify(values, null, 2));
-                }, 500);
-              }}
-              render={() => (
-              <Form>
-                <div className="form-group">
-                  <label htmlFor="clientName">User name</label>
-                  <Field name="clientName" placeholder="Enter your name" />
-                </div>
-
-                <div className="form-group">
-                  <label htmlFor="value">Task value</label>
-                  <Field name="value" placeholder="Enter task value" />
-                </div>
-
-                <div className="form-group">
-                  <label htmlFor="type">Task value</label>
-                    <select className="form-control" name="type">
-                      <option value="cubic job">Cubic job</option>
-                      <option value="flat job">Flat job</option>
-                    </select>
-                </div>
-
-                <div className="form-group">
-                  <label htmlFor="gender">Task value</label>
-                    <select className="form-control" name="gender">
-                      <option value="male">Male</option>
-                      <option value="female">Female</option>
-                    </select>
-                </div>
-                <button type="submit">Submit</button>
-              </Form>
-            )}/>
+            <MyEnhancedForm/>
           </Modal.Body>
         </Modal>
       </div>
